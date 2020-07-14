e493391da3c13a2b6b26748d50295123283ec33d
ernifang0563 <66763368+ernifang0563@users.noreply.github.com>
2020-07-14 14:20:31 +0700

    menambahkan int main

--- TR_GRAFKOM_672018299.cpp
+++ TR_GRAFKOM_672018299.cpp
@@ -12,3 +12,17 @@ void ukuran(int, int);
 void mouse (int button, int state, int x, int y);
 void ukuran(int, int);
 void mouseMotion(int x, int y);
+
+
+int main(int argc, char** argv) 
+{
+	glutInit(argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(250, 250);
+	glutInitWindowPosition(200, 200);
+	glutCreateWindow("TR_GRAFKOM_672018299");
+	Initialize();
+	glutDisplayFunc(Draw);
+	glutMainLoop();
+	return 0;
+}
