90633b1eb5339b13855621b5d9b4284ba92a59fd
elvira1412 <672018052@student.uksw.edu>
2020-07-14 13:21:03 +0700

    akkkk

--- TR_GRAFKOM_672018299.cpp
+++ TR_GRAFKOM_672018299.cpp
@@ -4,3 +4,11 @@
 #include<GL/glew.h>
 #include<GL/freeglut.h>
 #include<iostream>
+
+void init (void);
+void tampil (void);
+void keyboard(unsigned char, int, int);
+void ukuran(int, int);
+void mouse (int button, int state, int x, int y);
+void ukuran(int, int);
+void mouseMotion(int x, int y);
